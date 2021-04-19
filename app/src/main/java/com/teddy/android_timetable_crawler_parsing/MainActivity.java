2e71805d080bb06c59fc322049eef1873b7d5895
Teddy <2053518437@qq.com>
2021-04-19 20:40:40 +0800

    4-19重写进度++

--- app/src/main/java/com/teddy/android_timetable_crawler_parsing/MainActivity.java
+++ app/src/main/java/com/teddy/android_timetable_crawler_parsing/MainActivity.java
@@ -2,6 +2,7 @@ package com.teddy.android_timetable_crawler_parsing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, activity_login.class);
+        startActivity(intent);
     }
 }
